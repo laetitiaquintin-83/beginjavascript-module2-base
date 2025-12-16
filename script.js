3f02de6b10658a2005c9a48d9d15746e94171457
laetitiaquintin-83 <laetitia.quintin@laplateforme.io>
2025-12-16 16:06:34 +0100

    fix(script.js): corriger l'appel à console.log pour afficher "Hello World!"

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 // ℹ️ La propriété console.log() affiche un message dans la console Web.
 // ℹ️ Elle prend en paramètre une string (on verra plus tard ce que c'est) et l'affiche dans la console.
 // 🦁 Ta mission est d'afficher : Hello World!
+.console.log('hello word!');
+/*sert a afficher hello word*/
